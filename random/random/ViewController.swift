0f2088569a5d1bb33dffdcae4a9dd20e0db1a890
55011212069thongchai <imac@msu>
2014-09-26 17:41:26 +0700

    random
    random

---
+++ random/random/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  random
+//
+//  Created by Student on 9/26/14.
+//  Copyright (c) 2014 random. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lable1: UILabel!
+    @IBOutlet weak var lable2: UILabel!
+    @IBOutlet weak var lable3: UILabel!
+    @IBOutlet weak var reset: UIButton!
+    
+    var i=0, j=0, k=0
+    
+    @IBAction func one(sender: AnyObject) {
+    }
+    
+    @IBAction func two(sender: AnyObject) {
+    }
+    
+    @IBOutlet weak var three: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
