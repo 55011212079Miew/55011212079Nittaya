00939c2f6c0df47be4317036aad883a227444550
deminette <student@tests-iMac-108.local>
2014-10-22 16:27:50 +0700

    m
    m

--- exam/ColorTwoViewController.swift
+++ exam/ColorTwoViewController.swift
@@ -7,20 +7,33 @@
 //
 
 import UIKit
+protocol ColorTwoViewControllerDelegate{
+    func myVCDidFinish(controller:ColorTwoViewController,text:String)
+}
 
 class ColorTwoViewController: UIViewController {
     
-    var colorString = ""
+    var delegate:ColorTwoViewControllerDelegate? = nil
+    
+    var colorString = " "
 
-    @IBAction func ColorSelectionButton(sender: AnyObject) {
+    @IBAction func colorSelectionButton(sender: UIButton) {
+    
+        colorLabel.text = sender.titleLabel!.text
     }
-    @IBOutlet var colorLabel: UILabel!
-    @IBAction func savecolor(sender: UIBarButtonItem) {
+   
+
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    @IBAction func saveColor(sender: UIBarButtonItem) {
+            if(delegate != nil){
+            delegate!.myVCDidFinish(self, text: colorLabel!.text!)
+            
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        colorLabel.text = colorString
     }
 
     override func didReceiveMemoryWarning() {
--- exam/exam/ViewController.swift
+++ exam/exam/ViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ColorTwoViewControllerDelegate{
     
-    @IBOutlet var ColorLabel: UILabel!
+   
+    @IBOutlet var colorLabel: UILabel!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
        
+    }
+    func myVCDidFinish(controller: ColorTwoViewController, text: String){
+        colorLabel.text = "Co: " + text
+        controller.navigationController?.popViewControllerAnimated(true)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "mySegue"{
+            let vc = segue.destinationViewController as ColorTwoViewController
+            vc.colorString = colorLabel.text!
+            vc.delegate = self
+        }
     }
 
     
